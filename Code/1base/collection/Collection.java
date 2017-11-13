27ebfde86d0961ee02563e3d9269a479a859b1a5
qushuaisheng <846907369@qq.com>
2017-11-13 10:15:36 +0800

    collection

---
+++ Code/1base/collection/Collection.java
@@ -0,0 +1,10 @@
+package collection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Collection {
+	public static void main(String[] args){
+		List<String> list = new ArrayList<String>();
+	}
+}
