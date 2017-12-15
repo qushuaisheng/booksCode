7e92226919671eac59dabe113471451d3ca2272f
qss <qss@LAPTOP-0G2SUOE3>
2017-12-15 17:28:28 +0800

    添加说明。

--- Code/01base/classDemo/Test.java
+++ Code/01base/classDemo/Test.java
@@ -10,6 +10,17 @@ public class Test {
 	//Java（反射）获取Class对象的三种方法
 	@SuppressWarnings("unused")
 	private static void test1() throws Exception{
+		//常用第三种
+		//1.根据对象的引用.getClass()方法获取
+		// 1)需要创建对象，静态块和动态构造块均会执行
+		// 2)对象都有了还要反射干什么
+		//2.根据类名.class获取
+		// 1)不执行静态块和动态构造块
+		// 2)需要导入类的包，即：.java文件。依赖太强，不导包就抛编译错误
+		//3.根据Class中的静态方法Class.forName(); 
+		// 1）执行静态块、不执行动态构造块
+		// 2)一个字符串可以传入也可写在配置文件中等多种方法。
+		
 		//1.根据对象的引用.getClass()方法获取
 		//需要创建对象，静态块和动态构造块均会执行
 		Student student = new Student();
