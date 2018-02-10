b6d2dd922ed9a402656c520be5fa782fce9abd02
qss <qss@yk-dz-2561603>
2018-02-10 15:59:52 +0800

    添加了对第一个字符串的输出。

---
+++ Code/11/chapter03/String编码.java
@@ -0,0 +1,39 @@
+package chapter03;
+
+import java.io.UnsupportedEncodingException;
+
+import javax.xml.bind.DatatypeConverter;
+
+public class String编码 {
+	public static void main(String[] args){
+		encode();
+	}
+	
+	public static void encode(){
+//		String name = "I am 寳山";
+		String name = "I am 君山";
+		byte[] defaultByte = name.getBytes();
+		toHex(defaultByte);
+		
+		try{
+			byte[] iso8859 = name.getBytes("ISO-8859-1");
+			toHex(iso8859);
+			byte[] gb2312 = name.getBytes("GB2312");
+			toHex(gb2312);
+			byte[] gbk = name.getBytes("GBK");
+			toHex(gbk);
+			byte[] utf16 = name.getBytes("UTF-16");
+			toHex(utf16);
+			byte[] utf8 = name.getBytes("UTF-8");
+			toHex(utf8);
+		}catch(UnsupportedEncodingException e){
+			e.printStackTrace();
+		}
+	}
+
+	private static String toHex(byte[] byteArr){
+		String returnStr = DatatypeConverter.printHexBinary(byteArr);
+		System.out.println(returnStr);
+        return returnStr;  
+	}
+}
